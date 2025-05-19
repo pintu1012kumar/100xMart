e400c023686f40a26875ed7501466c01d7f8404a
Pintu kumar <pintu1012kumar@gmail.com>
2025-05-19 23:33:53 +0530

    add sellerdashboard

--- src/app/sellerdashboard/page.tsx
+++ src/app/sellerdashboard/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const sellerdashboard = () => {
+const SellerDashboard = () => {
   return (
-    <div>
-      sellerdashboard
+    <div className="flex items-center justify-center min-h-screen bg-base-200">
+      <div className="text-center max-w-md">
+        <h1 className="text-5xl font-bold">Hello there</h1>
+        <p className="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+          exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button className="bg-black">Get Started</button>
+      </div>
     </div>
   )
 }
 
-export default sellerdashboard
+export default SellerDashboard
