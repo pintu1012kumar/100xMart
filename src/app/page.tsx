2b8ea1dbfc3e94a43693a39be793c3bdb20ee944
Pintu kumar <pintu1012kumar@gmail.com>
2025-07-09 10:07:15 +0530

    page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+import ReverseExample from "@/components/Axischartbar";
 import { HeroSectionOne } from "../components/Herosection";
 import { AnimatedTooltipPreview } from "../components/Topseller";
 import Chartdetails from "@/components/Chartdetails";
 import { DraggableCardDemo } from "@/components/CountryName";
 import Footer from "@/components/Footer";
+import { CompareDemo } from "@/components/CompareDemo";
 
 export default function Home() {
   return (
@@ -13,6 +16,14 @@ export default function Home() {
         <AnimatedTooltipPreview />
       </div>
       <Chartdetails />
+      <div className="mt-20 mx-20 my-20">
+        <h1 className="text-center text-4xl font-bold ">
+        Buyer & Seller Page here !!
+        </h1>
+        <div className="flex justify-center items-center mt-20 mb-20 px-20">
+          <CompareDemo />
+        </div>
+      </div>
       <div className="mt-20">
         <h1 className="text-center text-4xl font-bold ">
           Top Buyer & Seller Countries :
@@ -21,9 +32,17 @@ export default function Home() {
           <DraggableCardDemo />
         </div>
       </div>
-      <div className="my-10 mx-10"><Footer /></div>
-
-     
+      <div className="mt-20 mx-20 my-20">
+        <h1 className="text-center text-4xl font-bold ">
+          Monthly Buyer & Seller :
+        </h1>
+        <div className="mt-20 mx-20 my-20">
+          <ReverseExample />
+        </div>
+      </div>
+      <div className="my-10 mx-10">
+        <Footer />
+      </div>
     </div>
   );
 }
